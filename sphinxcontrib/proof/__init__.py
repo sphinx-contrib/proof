e65dfefd9fa3d9e7b1b806ef77f5bc53ac9435ef
Louis Paternault <spalax@gresille.org>
2015-09-25 20:28:21 +0200

    Created sphinxcontrib.proof package

--- sphinxcontrib/proof/__init__.py
+++ sphinxcontrib/proof/__init__.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 
-# Copyright 2014-2015 Louis Paternault
+# Copyright 2015 Louis Paternault
 #
-# This file is part of Jouets.
-#
-# Jouets is free software: you can redistribute it and/or modify
+# Sphinxcontrib-Proof is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Jouets is distributed in the hope that it will be useful,
+# Sphinxcontrib-Proof is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Jouets.  If not, see <http://www.gnu.org/licenses/>.
+# along with Sphinxcontrib-Proof.  If not, see <http://www.gnu.org/licenses/>.
 
-"""TODO Update comments and documentation"""
+"""Provide tools to typeset theorems, proofs, etc. in Sphinx documentation."""
 
 import os
 import pkg_resources
