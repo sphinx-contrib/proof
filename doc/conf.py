9a99c73e1d9f9f72d0ae31002038d5182d4d1d4c
Louis Paternault <spalax@gresille.org>
2018-10-28 18:28:45 +0100

    [doc] Improve LaTeX rendering

--- doc/conf.py
+++ doc/conf.py
@@ -231,7 +231,7 @@ proof_latex_notheorem = ["proof"]
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, "Proof.tex", "Proof Documentation", "Louis Paternault", "manual")
+    (master_doc, "Proof.tex", "Proof Documentation", "Louis Paternault", "howto")
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -243,10 +243,10 @@ latex_logo = "_static/logo.pdf"
 # latex_use_parts = False
 
 # If true, show page references after internal links.
-# latex_show_pagerefs = False
+latex_show_pagerefs = True
 
 # If true, show URL addresses after external links.
-# latex_show_urls = False
+latex_show_urls = "footnote"
 
 # Documents to append as an appendix to all manuals.
 # latex_appendices = []
